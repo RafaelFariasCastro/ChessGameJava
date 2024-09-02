1298a21a584fd2276b4abfa4344b607efebfb345
Rafael Farias <rafaelfcastro.eng@gmail.com>
2024-09-02 17:19:24 -0300

    Second commit: Board

---
+++ src/boardgame/Board.java
@@ -0,0 +1,33 @@
+package boardgame;
+
+public class Board {
+
+	private int row;
+	private int coluns;
+	private Piece[][] pieces;
+	
+	public Board(int rows, int columns) {
+		
+		this.row = rows;
+		this.coluns = columns;
+		pieces = new Piece[rows][columns];
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColuns() {
+		return coluns;
+	}
+
+	public void setColuns(int coluns) {
+		this.coluns = coluns;
+	}
+		
+	
+}
