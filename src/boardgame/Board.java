6da78eb32b6c4a4bc0e90c2b4a4bd957f06e569f
Rafael Farias <rafaelfcastro.eng@gmail.com>
2024-09-10 22:40:05 -0300

    Tenth commit:BoardException.java

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,7 +8,7 @@ public class Board {
 	
 	public Board(int rows, int columns) {
 		if(rows < 1 || columns < 1) {
-			throw new BoardExeption("Sorry, there musty be at least 1 row and 1 column");
+			throw new BoardExeption("Sorry, there must be at least 1 row and 1 column");
 		}
 		this.rows = rows;
 		this.columns = columns;
@@ -32,6 +32,9 @@ public class Board {
 	}
 	
 	public Piece piece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardExeption("Position not on the board");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
@@ -51,6 +54,9 @@ public class Board {
 	}
 	
 	public boolean therIsAPiece(Position position) {
+		if(!positionExists(row, column)) {
+			throw new BoardExeption("Position not on the board");
+		}
 		return piece(position) != null;
 	}
 }
